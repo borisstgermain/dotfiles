374c29a7375354447a4446137470f18c14e9e0a1
borisstgermain <gallacius@gmail.com>
2025-05-03 17:32:22 +0300

    ignore file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,5 +2,8 @@
 (setq auto-save-list-file-name nil)
 (setq auto-save-default nil)
 
+;; Увеличить лимит памяти
+(setq gc-cons-threshold (* 50 1000 1000))
+
 ;; Загрузка кастомных модулей
 (load-file (expand-file-name "config/keybindings.el" user-emacs-directory))
